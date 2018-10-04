c089b6b72e1cfad8e82d562733e3b10615cc0746
Elmion <iight@bk.ru>
2018-10-04 14:35:09 +1000

    Добавлен Интефейс конектов

---
+++ GameTester/GameTester/Hero/IConnectEngine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTester.Hero
+{
+    interface IConnectEngine
+    {
+        bool Have(string Name);
+        int  Count(string Name);
+        bool Use(string Name, int Count);
+        void Add(string Name, int Count);
+        bool Remove(string Name, int Count);
+    }
+    interface ISaveLoad
+    {
+        bool Save();
+        bool Load();
+    }
+}
