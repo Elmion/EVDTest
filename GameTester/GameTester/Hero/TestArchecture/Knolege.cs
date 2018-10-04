094c7a11cd2ccbb663a4c57a2a5ab986a7f75edb
Евгений <firegeometry@gmail.com>
2018-10-05 00:04:50 +1000

    олролр

---
+++ GameTester/GameTester/Hero/TestArchecture/Knolege.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTester.Hero.TestArchecture
+{
+    class Knowledge
+    {
+        KnowledgeItem root; 
+    }
+    class KnowledgeItem
+    {
+        string Name; //Название знания
+        KnowledgeItem parent; //Предок
+        List<KnowledgeItem> childs;//Другие науки и навыки происходящие из даного
+        List<Charter> charters; //Части знания
+        Dictionary<KnowledgeItem, float> RequestForAccess; //Минимальные требования для доступа
+        float CalcAmountKnow() //Процент выучиности раздела. Нужен для пропуска в следующий раздел
+        {
+            throw new NotImplementedException();
+        }
+        bool AccessGrant()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    internal class Charter
+    {
+        string Name;//Название раздела
+        bool Theoretical;//Теоретическое знание
+        float ProcenteKnow; //Процент выучиности
+    }
+    class Evelopment
+    {
+        int Temprature;
+        float Lightness = 1.0f; //ярко
+        List<EvelopmentItem> Items; //Предметы находящиеся в данной локации
+    }
+    internal class EvelopmentItem
+    {
+        List<EvelopmentItem> InnerItems; //Предметы внути предмета
+        string Name;
+    }
+    class Inventory
+    {
+
+    }
+    class Resources
+    {
+
+    }
+    class Story
+    {
+        
+    }
+}
